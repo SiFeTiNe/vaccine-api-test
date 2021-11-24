bd7a93c809719489b2802c849bd1e15361d5e6cc
keyboard2543 <keyboard2543@gmail.com>
2021-11-24 08:07:25 +0700

    Missing attributes testing

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Getting start:
 - You need [Node JS](https://nodejs.org/) installed in your machine.
-- Create a folder or cd into the foler you would like to store this repository.
-- `> git clone https://github.com/keyboard2543/api-testing`
+- Create a folder or cd into the folder you would like to store this repository.
+- `> git clone https://github.com/keyboard2543/vaccine-api-test`
 - cd into your cloned local repository.
 - `> npm install`
 
--- test/test.js
+++ test/test.js
@@ -55,21 +55,13 @@ describe('WCG API Tests', () => {
     /**
      * Setup before the test is started
      */
-    before((done) => {
-        chai.request(server)                
-        .post('/registration')
-        .set('content-type', 'application/x-www-form-urlencoded')
-        .send(kleeJSON)
+        before((done) => {
+        chai.request(server)
+        .delete(`/registration/${kleeJSON['citizen_id']}`)
         .end((err, response) => {
-            chai.request(server)                
-                .post('/registration')
-                .set('content-type', 'application/x-www-form-urlencoded')
-                .send(enfjJSON)
-                .end((err, response) => {
-                    done();
-                });
+            done();
         });
-    });
+    })
 
     /**
      * Test the GET registration
@@ -82,11 +74,23 @@ describe('WCG API Tests', () => {
          */
         before((done) => {
             chai.request(server)
-                .get(`/registration/${kleeJSON['citizen_id']}`)
-                .end((err, response) => {
-                    this.res_data = JSON.parse(response.text);
-                    done();
-                });
+            .post('/registration')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(kleeJSON)
+            .end((err, response) => {
+                chai.request(server)
+                    .post('/registration')
+                    .set('content-type', 'application/x-www-form-urlencoded')
+                    .send(enfjJSON)
+                    .end((err, response) => {
+                        chai.request(server)
+                        .get(`/registration/${kleeJSON['citizen_id']}`)
+                        .end((err, response) => {
+                            this.res_data = JSON.parse(response.text);
+                            done();
+                        });
+                    });
+            });
         });
         
         for (const [key, value] of Object.entries(kleeJSON)) {
@@ -106,7 +110,7 @@ describe('WCG API Tests', () => {
          * Setup before the test is started
          */
         before((done) => {
-            chai.request(server)                
+            chai.request(server)
             .delete(`/registration/${mockerJSON['citizen_id']}`)
             .end((err, response) => {
                 done();
@@ -127,7 +131,7 @@ describe('WCG API Tests', () => {
         it('POST /registration with invalid citizen_id length', (done) => {
             let mockeryJSON = Object.assign(this, mockerJSON);
             mockeryJSON['citizen_id'] = '1150';
-            chai.request(server)                
+            chai.request(server)
             .post('/registration')
             .set('content-type', 'application/x-www-form-urlencoded')
             .send(mockeryJSON)
@@ -140,7 +144,7 @@ describe('WCG API Tests', () => {
         it('POST /registration with invalid citizen_id format', (done) => {
             let mockeryJSON = Object.assign(this, mockerJSON);
             mockeryJSON['citizen_id'] = '~~~ kore wa id janain da ~~~';
-            chai.request(server)                
+            chai.request(server)
             .post('/registration')
             .set('content-type', 'application/x-www-form-urlencoded')
             .send(mockeryJSON)
@@ -153,7 +157,7 @@ describe('WCG API Tests', () => {
         it('POST /registration with invalid age', (done) => {
             let mockeryJSON = Object.assign(this, mockerJSON);
             mockeryJSON['birth_date'] = `${year-7}-01-01`;
-            chai.request(server)                
+            chai.request(server)
             .post('/registration')
             .set('content-type', 'application/x-www-form-urlencoded')
             .send(mockeryJSON)
@@ -166,7 +170,7 @@ describe('WCG API Tests', () => {
         it('POST /registration with invalid month in birth_date', (done) => {
             let mockeryJSON = Object.assign(this, mockerJSON);
             mockeryJSON['birth_date'] = `${year-19}-19-01`;
-            chai.request(server)                
+            chai.request(server)
             .post('/registration')
             .set('content-type', 'application/x-www-form-urlencoded')
             .send(mockeryJSON)
@@ -179,7 +183,7 @@ describe('WCG API Tests', () => {
         it('POST /registration with invalid day in birth_date', (done) => {
             let mockeryJSON = Object.assign(this, mockerJSON);
             mockeryJSON['birth_date'] = `${year-19}-12-79`;
-            chai.request(server)                
+            chai.request(server)
             .post('/registration')
             .set('content-type', 'application/x-www-form-urlencoded')
             .send(mockeryJSON)
@@ -192,7 +196,7 @@ describe('WCG API Tests', () => {
         it('POST /registration with invalid birth_date format', (done) => {
             let mockeryJSON = Object.assign(this, mockerJSON);
             mockeryJSON['birth_date'] = 'tanjoubi-da-yo';
-            chai.request(server)                
+            chai.request(server)
             .post('/registration')
             .set('content-type', 'application/x-www-form-urlencoded')
             .send(mockeryJSON)
@@ -202,10 +206,12 @@ describe('WCG API Tests', () => {
             });
         });
 
-        it('POST /registration name with a script', (done) => {
+        it('POST /registration with a html tag in name', (done) => {
             let mockeryJSON = Object.assign(this, mockerJSON);
-            mockeryJSON['name'] = "Script is here!<script>alert('Kleeboard is Too horny!');</script>";
-            chai.request(server)                
+            mockeryJSON['citizen_id'] = '0126210545460'
+            mockeryJSON['name'] = `<a href="https://libredd.it/img/8s4cuxfboxd71.jpg">Horny INFP belike(2)</a>`;
+            mockeryJSON['surname'] = '<- Click';
+            chai.request(server)
             .delete(`/registration/${mockeryJSON['citizen_id']}`)
             .set('content-type', 'application/none')
             .end((err, response) => {
@@ -219,6 +225,84 @@ describe('WCG API Tests', () => {
                 });
             });
         });
+
+        it('POST /registration with a html tag in surname', (done) => {
+            let mockeryJSON = Object.assign(this, mockerJSON);
+            mockeryJSON['citizen_id'] = '0126210545459'
+            mockeryJSON['name'] = "Horny INFP belike(1):";
+            mockeryJSON['surname'] = `<a href="https://i.redd.it/3zen79gyj0y51.png">Click!</a>`;
+            chai.request(server)
+            .delete(`/registration/${mockeryJSON['citizen_id']}`)
+            .set('content-type', 'application/none')
+            .end((err, response) => {
+                chai.request(server)                
+                .post('/registration')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send(mockeryJSON)
+                .end((err, response) => {
+                    (JSON.parse(response.text)).should.have.property('feedback').eq(REGISTRATION_FEEDBACK['other']);
+                    done();
+                });
+            });
+        });
+
+        it('POST /registration with a html tag in address', (done) => {
+            let mockeryJSON = Object.assign(this, mockerJSON);
+            mockeryJSON['citizen_id'] = '0126210545458'
+            mockeryJSON['name'] = `Mediator`;
+            mockeryJSON['surname'] = 'Over there! ->';
+            mockeryJSON['occupation'] = `<a href="https://libredd.it/img/8s4cuxfboxd71.jpg">Horny INFP belike(3)</a>`
+            chai.request(server)
+            .delete(`/registration/${mockeryJSON['citizen_id']}`)
+            .set('content-type', 'application/none')
+            .end((err, response) => {
+                chai.request(server)                
+                .post('/registration')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send(mockeryJSON)
+                .end((err, response) => {
+                    (JSON.parse(response.text)).should.have.property('feedback').eq(REGISTRATION_FEEDBACK['other']);
+                    done();
+                });
+            });
+        });
+
+        it('POST /registration with a html tag in occupation', (done) => {
+            let mockeryJSON = Object.assign(this, mockerJSON);
+            mockeryJSON['citizen_id'] = '0126210545457'
+            mockeryJSON['name'] = `INFP`;
+            mockeryJSON['surname'] = 'The idealist';
+            mockeryJSON['occupation'] = `Fi dom, Ne aux`
+            mockeryJSON['address'] = '<a href="https://i.redd.it/ib6ot6pwf3k71.jpg">Please forgot what I have mentioned!</a>'
+            chai.request(server)
+            .delete(`/registration/${mockeryJSON['citizen_id']}`)
+            .set('content-type', 'application/none')
+            .end((err, response) => {
+                chai.request(server)                
+                .post('/registration')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send(mockeryJSON)
+                .end((err, response) => {
+                    (JSON.parse(response.text)).should.have.property('feedback').eq(REGISTRATION_FEEDBACK['other']);
+                    done();
+                });
+            });
+        });
+
+        for (const [key, value] of Object.entries(mockerJSON)) {
+            it(`POST /registration with missing ${key}`, (done) => {
+                let mockeryJSON = Object.assign(this, mockerJSON);
+                delete mockeryJSON[key];
+                chai.request(server)
+                .post('/registration')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send(mockeryJSON)
+                .end((err, response) => {
+                    (JSON.parse(response.text)).should.have.property('feedback').not.eq(REGISTRATION_FEEDBACK['missing_key']);
+                    done();
+                });
+            });
+        }
     });
     
 });
